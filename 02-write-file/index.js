3296e1b21a3d92ad6b20e5aa0251a8a9b3917b6a
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-05-19 13:22:57 -0400

    feat: add solution to task - 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+
+const writeableStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Привет, напиши мне что-нибудь:\n');
+stdin.on('data', data => {
+  if(data.toString().trim() !== 'exit'){
+    writeableStream.write(data.toString());
+  }
+  else {
+    //stdout.write('Спасибо, пока-пока!');
+    process.exit();
+  }stdout.write('Напиши мне что-нибудь еще:\n'); 
+});
+process.on('exit', () => {stdout.write('С тобой было очень интересно!!! Увидимся еще!');});process.on('SIGINT', () => {process.exit();
+});
