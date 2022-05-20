6e2544c7dd79c0329189487c9467e58f78289332
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-05-19 20:08:49 -0400

    feat: add solution to task - 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,49 @@
+const fs = require('fs');
+const path = require('path');
 
+const original = path.join(__dirname, 'files');
+const copyFolder = path.join(__dirname, 'files-copy');
+
+const newDirectory = (original, copyFolder) => {
+  fs.mkdir(copyFolder, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log('Копия папки была создана, можете проверить - она действительно существует!!!');
+  });
+
+  fs.readdir(copyFolder, (err, items) => {
+    if (err)
+      console.log(err);
+    else {
+      items.forEach(item => {
+        fs.access(path.join(original, item), (err) => {
+          if (err) {
+            fs.rm(path.join(copyFolder, item), { recursive: true, force: true}, (err)   => {
+              if (err) throw err;
+            });
+          }
+        });
+      });
+    }
+  });
+
+  fs.readdir(original, {withFileTypes: true}, (err, items) => {
+    if (err)
+      console.log(err);
+    else {
+      items.forEach(item => {
+        if(item.isFile()){
+          fs.copyFile(path.join(original, item.name), path.join(copyFolder, item.name), (err) => {
+            if (err) throw err;
+          });
+        }
+        if(item.isDirectory()) {
+          newDirectory(path.join(original, item.name), path.join(copyFolder, item.name));
+        }
+      });
+    }
+  });
+};
+
+newDirectory(original, copyFolder);
